ebf043623c55cf8a445b7b7e269daf6db1882ee4
mlamsey <31256490+mlamsey@users.noreply.github.com>
2024-11-27 12:01:25 -0500

    checkin label changer

--- src/zeste_vision/data_tools/video_labels.py
+++ src/zeste_vision/data_tools/video_labels.py
@@ -200,8 +200,71 @@ def get_labels(df: pd.DataFrame, outfile: str = "labels2.csv"):
 
                     # f.write(f"{user_id},{ex_id},{i + 1},{error}\n")
 
-def train_eval_split_labels():
-    pass
+def train_eval_split_labels(parent_labels: str, top_dir: str):
+    """
+    Args:
+        parent_labels: path to the parent labels file
+        top_dir: path to the top directory containing the videos (train/test are subdirectories)
+    """
+    # config
+    video_header_len = 21
+
+    # checks
+    if not os.path.exists(parent_labels):
+        print(f"File {parent_labels} does not exist.")
+        return
+
+    if not os.path.exists(top_dir):
+        print(f"Directory {top_dir} does not exist.")
+        return
+
+    # load labels
+    df = pd.read_csv(parent_labels)
+
+    train_dir = os.path.join(top_dir, "train")
+    eval_dir = os.path.join(top_dir, "eval")
+
+    new_df = pd.DataFrame(columns=["File Path", "Error"])
+
+    # train labels
+    train_participants = os.listdir(train_dir)
+    train_participants = [p for p in train_participants if "zst" in p]
+    train_participants.sort()
+
+    for p in train_participants:
+        participant_dir = os.path.join(train_dir, p)
+        videos = os.listdir(participant_dir)
+        videos = [v for v in videos if v.endswith(".mp4")]
+        videos.sort()
+
+        for exercise in ["seated_reach_forward_low", "seated_forward_kick", "seated_calf_raises", "standing_reach_across", "standing_windmills", "standing_high_knees"]:
+            exercise_videos = [v for v in videos if exercise in v]
+            labels = df[(df["Participant ID"] == p.upper()) & (df["Exercise"] == exercise)]
+
+            headers = [v[:video_header_len] for v in exercise_videos]
+            unique_headers = list(set(headers))
+            unique_headers.sort()
+
+            for i, header in enumerate(unique_headers):
+                set_videos = [v for v in exercise_videos if v.startswith(header)]
+                set_videos.sort()
+                for v in set_videos:
+                    try:
+                        file_path = os.path.join(participant_dir, v)
+                        error = labels["Error"].values[i]
+
+                        # print(f"{file_path}: {error}")
+
+                        new_row = pd.DataFrame({"File Path": [file_path], "Error": [error]})
+                        
+                        # add row
+                        new_df = pd.concat([new_df, new_row], ignore_index=True)
+                    except Exception as e:
+                        print(e)
+                        print(f"Error with {file_path}")
+                        print(f"Error labels: {labels["Error"].values}")
+    
+    new_df.to_csv(os.path.join(top_dir, "train_labels.csv"), index=False)
 
 ###
 def test_get_set_cols_bool(args):
@@ -239,12 +302,18 @@ def test_visualize_proportions_of_yes_no(args):
     df = load_data(args.file)
     visualize_proportions_of_yes_no(df)
 
+def test_train_eval_split_labels(args):
+    parent_labels = "labels_parent.csv"
+    top_dir = "/nethome/mlamsey3/Documents/data/zeste_studies/form_feedback/split_videos"
+    train_eval_split_labels(parent_labels, top_dir)
+
 def main(args):
     if args.test:
         # test_get_labels_ex_set(args)
         # test_visualize_proportions_of_yes_no(args)
         # test_get_set_cols_err_type(args)
-        test_df_main(args)
+        # test_df_main(args)
+        test_train_eval_split_labels(args)
     elif args.labels:
         df = load_data(args.file)
         get_labels(df)
