d4c11ab4d4f2351d02a4c61267b57868b6fc1461
mlamsey <31256490+mlamsey@users.noreply.github.com>
2024-12-02 17:49:48 -0500

    checkin model oops

--- src/zeste_vision/models/feedback.py
+++ src/zeste_vision/models/feedback.py
@@ -1,21 +1,36 @@
+import os
 import torch
+
+from zeste_vision.data_tools.dataloader import PoseDateset
 
 class FeedbackModel(torch.nn.Module):
     def __init__(self):
         super(FeedbackModel, self).__init__()
 
         # architecture
-        self.conv1 = torch.nn.Conv3d(in_channels=1,
+        self.conv1 = torch.nn.Conv2d(in_channels=5,
                                      out_channels=16,
-                                     kernel_size=(3, 3, 3),
-                                     stride=(1, 1, 1))
+                                     kernel_size=(3, 3),
+                                     stride=1)
         
-        self.conv2 = torch.nn.Conv3d(in_channels=16,
+        self.maxpool1 = torch.nn.MaxPool2d(kernel_size=(2, 2),
+                                           stride=2)
+        
+        self.conv2 = torch.nn.Conv2d(in_channels=16,
                                      out_channels=16,
-                                     kernel_size=(3, 3, 1),
-                                     stride=(1, 1, 1))
+                                     kernel_size=(3, 3),
+                                     stride=1)
         
-        flattened_size = 16 * 11 * 29 * 1
+        self.maxpool2 = torch.nn.MaxPool2d(kernel_size=(2, 2),
+                                           stride=2)
+        
+        # self.conv_layers = [self.conv1, self.conv2]
+        # self.pool_layers = [self.maxpool1, self.maxpool2]
+        self.conv_layers = [self.conv1]
+        self.pool_layers = [self.maxpool1]
+
+        # flattened_size = 576
+        flattened_size = 3360
         self.fc1 = torch.nn.Linear(flattened_size, 1)
 
         # activations
@@ -23,12 +38,12 @@ class FeedbackModel(torch.nn.Module):
         self.sigmoid = torch.functional.F.sigmoid
 
     def forward(self, x):
-        conv_layers = [self.conv1, self.conv2]
-        for conv in conv_layers:
+        for conv, pool in zip(self.conv_layers, self.pool_layers):
             x = self.relu(conv(x))
+            x = pool(x)
 
         # flatten for fc layer
-        x = x.view(x.size(0), -1)
+        x = torch.flatten(x, start_dim=1)
         x = self.fc1(x)
 
         x = self.sigmoid(x)
@@ -36,7 +51,14 @@ class FeedbackModel(torch.nn.Module):
         return x
 
 def test_model_forward():
-    input_data = torch.randn(1, 1, 15, 33, 3)
+    # dataset
+    data_dir = "/nethome/mlamsey3/Documents/data/zeste_studies/form_feedback/split_poses/train"
+    csv_file = os.path.join(data_dir, "train_labels.csv")
+    dataset = PoseDateset(csv_file)
+
+    input_data = dataset[123][0]
+    input_data = input_data.unsqueeze(0)
+    
     model = FeedbackModel()
     try:
         output = model(input_data)
